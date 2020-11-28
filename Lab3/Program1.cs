d4d243e804c0200720b2d5c2db194f3608cf9407
Lolens <75110249+Lolens@users.noreply.github.com>
2020-11-28 10:44:11 +0300

    Lab3

---
+++ Lab3/Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a > 0)
+            {
+                a = a * a * a;
+            }
+            else
+            {
+                a = a * a;
+            }
+            if (b > 0)
+            {
+                b = b * b * b;
+            }
+            else
+            {
+                b = b * b;
+            }
+            if (c > 0)
+            {
+                c = c * c * c;
+            }
+            else
+            {
+                c = c * c;
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Lab3/Program2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня (1-7)");
+            int day = Convert.ToInt16(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели неверные данные");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
