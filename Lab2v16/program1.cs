c9c7c0f0d958317d893d64ca27c2416ed9db0f36
Lolens <75110249+Lolens@users.noreply.github.com>
2020-11-30 17:45:21 +0300

    Lab2v16

---
+++ Lab2v16/program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите y, x, z");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+            double f;
+            f = Math.Pow(y, x) + (Math.Sqrt(Math.Abs(x) + Math.Exp(y))) - (Math.Pow(z, 3) * Math.Pow(Math.Sin(y),2)) / (y + Math.Pow(z, 2) / (y - x));
+            Console.WriteLine(f);
+            Console.ReadKey();    
+        }
+    }
+}
